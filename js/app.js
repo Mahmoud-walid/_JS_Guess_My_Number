4ce11b737cd24e17fd0435d746a0e73658f97e2c
mahmmoud walid mahmoud <modywmbadr@gmail.com>
2023-06-20 17:25:33 +0300

    guess number

--- js/app.js
+++ js/app.js
@@ -1,16 +1,31 @@
 'use strict'
 const randomNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(randomNumber);
+let scoreElement = Number(document.querySelector(".score_txt .score").innerHTML)
+let score = Number(scoreElement)
 
-let score = Number(document.querySelector(".score_txt .score").innerHTML)
+
 function checkNumber() {
     const selectNumber = document.querySelector(".select_number").value
+    if (Number(selectNumber) !== randomNumber) {
+        if (score !== 0 || score === randomNumber) {
+            score -= 1;
+            document.querySelector(".score_txt .score").innerHTML = score
+        }
+    }
     if (Number(selectNumber) === randomNumber) {
         document.querySelector(".guessNumber").innerHTML = randomNumber;
         document.querySelector(".message").innerHTML = "🎉 Correct answer!"
         document.body.style.backgroundColor = "green"
+        document.querySelector(".high_score_txt .HighScore").innerHTML = score
     }
-    if (Number(selectNumber) !== randomNumber) {
-        score -= 1;
+    else if (selectNumber > randomNumber) {
+        document.querySelector(".message").innerHTML = "Too high!"
+    } else {
+        document.querySelector(".message").innerHTML = "Too low!"
     }
+}
+
+function againBtn() {
+    location.reload()
 }
