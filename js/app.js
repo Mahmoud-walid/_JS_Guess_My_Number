3934988fd06934c811259d0134c651f664c3445e
mahmmoud walid mahmoud <modywmbadr@gmail.com>
2023-06-20 15:15:30 +0300

    huess_number

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 'use strict'
+const randomNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(randomNumber);
+
+let score = Number(document.querySelector(".score_txt .score").innerHTML)
+function checkNumber() {
+    const selectNumber = document.querySelector(".select_number").value
+    if (Number(selectNumber) === randomNumber) {
+        document.querySelector(".guessNumber").innerHTML = randomNumber;
+        document.querySelector(".message").innerHTML = "🎉 Correct answer!"
+        document.body.style.backgroundColor = "green"
+    }
+    if (Number(selectNumber) !== randomNumber) {
+        score -= 1;
+    }
+}
